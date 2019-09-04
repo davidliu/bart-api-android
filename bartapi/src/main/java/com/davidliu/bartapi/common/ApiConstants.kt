6667bb564eea284e23d48c8fedb30b9d26b3463d
David Liu <dliu@medium.com>
2019-09-04 14:24:58 -0700

    Add bart base url to api constants

--- bartapi/src/main/java/com/davidliu/bartapi/common/ApiConstants.kt
+++ bartapi/src/main/java/com/davidliu/bartapi/common/ApiConstants.kt
@@ -4,5 +4,6 @@ class ApiConstants private constructor() {
     companion object {
         const val CDATA_SECTION = "#cdata-section"
         const val LEAVING = "Leaving"
+        const val API_BASE_URL = "https://api.bart.gov/api/"
     }
 }
